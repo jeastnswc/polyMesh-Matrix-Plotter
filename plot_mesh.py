ea56f4f9a23c90af732cbe11398732f864cc1aa0
Jonathan <jonathan.r.eastridge.civ@us.navy.mil>
2022-06-24 11:51:36 -0500

    Add ability to plot sparse matrices

--- plot_mesh.py
+++ plot_mesh.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.sparse as sps
 
 #casedir = '/home/eastridge/Documents/OpenFOAM/thrusterPod/run09/'
-casedir = ''
+casedir = 'sampleMeshes/'
 #f = open(casedir + 'constant/polyMesh-airfoilTRex/neighbour', 'r')
 #g = open(casedir + 'constant/polyMesh-airfoilTRex/owner', 'r')
 #f = open(casedir + 'constant/polyMesh-airfoilTRex-renumbered/neighbour', 'r')
 #g = open(casedir + 'constant/polyMesh-airfoilTRex-renumbered/owner', 'r')
-f = open(casedir + '1/polyMesh/neighbour', 'r')
-g = open(casedir + '1/polyMesh/owner', 'r')
+#f = open(casedir + 'polyMesh7x7/neighbour', 'r')
+#g = open(casedir + 'polyMesh7x7/owner', 'r')
+f = open(casedir + 'polyMesh-thruster/neighbour', 'r')
+g = open(casedir + 'polyMesh-thruster/owner', 'r')
 
 #f = open('constant/polyMesh/neighbour', 'r')
 #g = open('constant/polyMesh/owner', 'r')
@@ -21,11 +24,13 @@ while True:
     if '(' in line:
         break
 
-nCells = 3784 # just for airfoilTRex
+#nCells = 3784 # just for airfoilTRex
 
-matrix = np.zeros([nCells, nCells], dtype=int)
-np.fill_diagonal(matrix, 1)
+#matrix = np.zeros([nCells, nCells], dtype=int)
+#np.fill_diagonal(matrix, 1)
 
+cell1list = []
+cell2list = []
 while True:
     line_neighbour = f.readline()
     line_owner = g.readline()
@@ -36,18 +41,33 @@ while True:
     try:
         cell1 = int(line_neighbour)
         cell2 = int(line_owner)
-        matrix[cell1, cell2] = 1
-        matrix[cell2, cell1] = 1
+        cell1list.append(cell1)
+        cell2list.append(cell2)
+        cell1list.append(cell2)
+        cell2list.append(cell1)
+        #matrix[cell1, cell2] = 1
+        #matrix[cell2, cell1] = 1
     except:
         cells1 = [int(cell) for cell in line_neighbour.split()]
         cells2 = [int(cell) for cell in line_owner.split()]
         for i, cell1 in enumerate(cells1):
             cell2 = cells2[i]
-            matrix[cell1, cell2] = 1
-            matrix[cell2, cell1] = 1
+            cell1list.append(cell1)
+            cell2list.append(cell2)
+            cell1list.append(cell2)
+            cell2list.append(cell1)
+            #matrix[cell1, cell2] = 1
+            #matrix[cell2, cell1] = 1
 
+for i in range(nCells):
+    cell1list.append(i)
+    cell2list.append(i)
 
-plt.matshow(matrix)
+oneslist = np.ones(len(cell1list), dtype=int)
+matrix = sps.csr_matrix((oneslist, (cell1list, cell2list)))
+
+#plt.matshow(matrix)
+plt.spy(matrix)
 plt.tight_layout()
 plt.show()
 
